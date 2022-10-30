c953ba9a52dd2f1424708a3e08b66bd82d68bcf2
NizarDarwish <nizardarwish001@gmail.com>
2022-10-30 11:41:05 +0100

    done

--- Agent/agent.py
+++ Agent/agent.py
@@ -16,6 +16,7 @@ class Agent:
     tweets_atoms_memory = set([])
     ontology_properties_memory = set([])
     tweets_properties_memory = set([])
+    dict_ontology_properties = {}
     awaiting_stories = {}
 
     def __init__(self, ontology, tweets, folder_name):
@@ -39,6 +40,9 @@ class Agent:
         print(self.get_consequents(self.ontology.Running, "not isBadFor", -1))
         print("CHECK PROPERTY")
         print(self.check_property(self.ontology.Running, "usesBodyPart", self.ontology.Knee))
+        print(self.get_relations(self.ontology.Running))
+        print(self.process_story(""))
+
 
 
     def open_ontology(self, ontology):
@@ -89,21 +93,40 @@ class Agent:
             self.process_story(story)
             self.awaiting_stories.remove(story)
 
-    def classify_statements(statements, property):
-        return [], []
+    def classify_statements(self, statement):
+        found_negative = False
+        print(statement)
+        for property in range(1, len(statement), 2):
+            if self.dict_ontology_properties[statement[property]] == 'decrease':
+                found_negative = True
+
+        if found_negative:
+            return 'decrease'
+        else:
+            return 'increase'
+
+    
+    def make_linked_inferences(self, arguments, onto_consequents, tweet_consequents, domain, property, scores):
+        for linked_inferences in enumerate(arguments):
+            if domain == linked_inferences[1][1][-1]:
+                for cons in onto_consequents + tweet_consequents:
+                    Class = self.classify_statements(linked_inferences[1][1] + [property, cons[0]])
+                    arguments.append((scores[linked_inferences[0]] + linked_inferences[1][0], linked_inferences[1][1] + [property, cons[0]], Class))
+
+        return arguments
+
 
     def process_story(self, story):
         """
-        If you cant find P->Q directly then extract all P's relations (with all its properties), classify them
+        If you cant find P->Q directly then extract all P's relations (with all its properties) and classify them
         We only care to classify P's properties
 
         The loop should be: while the sources can bounce off eachother
-        e.g. P causes obesity (causes = enhances ++)
         """
         # Process the story
-        (object1, property, object2) = self.extract_query(story)
+        (object1, property, object2) = (self.ontology.Cookies, "hasNutrient", self.ontology.highSugars)#self.extract_query(story)
         # Check if the atoms are in the ontology:
-        if property not in self.ontology_properties_memory.union(self.tweets_properties_memory):
+        if property not in self.dict_ontology_properties.keys() and property not in self.tweets_properties_memory:
             print("Sorry, I don't know this property!")
             return
         if object1 not in self.ontology_atoms_memory.union(self.tweets_atoms_memory):
@@ -116,35 +139,51 @@ class Agent:
         # If both atoms are in the ontology, check if the property is in the ontology
         knowledge_base = {1: self.ontology_atoms_memory, -1: self.tweets_atoms_memory}
 
-        story_confirmed_by = []
-        story_denied_by = []
         story_true = False
+        arguments = [(0, [object1], "")]
+        complete_arguments = []
+        domains = [object1]
         for kb in knowledge_base:
-            if object1 in knowledge_base[kb] and object2 in knowledge_base[kb]:
-                # If the property is in the ontology, check which statements possitively/negatively influence the range
-                scores, statements = self.check_property(object1, property, object2, ontology_type=kb)
-                if statements:
-                    story_confirmed_by, story_denied_by = self.classify_statements(statements, property)
+            while domains:
+                print("Domain ", domains[0])
+                onto_scores, onto_properties = self.get_relations(domains[0], ontology_type=kb)
+                tweet_scores, tweet_properties = [], []#self.get_relations(domains[0], ontology_type=-1 * kb)
+                properties = onto_properties + tweet_properties
+                scores = onto_scores + tweet_scores
+                if properties:
+                    for p in properties:
 
-            elif object1 in knowledge_base[kb] and object2 in knowledge_base[-1 * kb]:
-                # If the objects are in two different ontologies, try to make a joint inference
-                print("I don't know, but I'll ask my friends!")
-                # look for all consequents with every property that is linked to object 1
-                scores, consequents = self.get_consequents(object1, property, ontology_type=kb)
+                        if isinstance(p, str):
+                            print("Prop from tweeter ", p)
+                            label = p
+                        else:
+                            print("Prop ", p.label[0])
+                            label = p.label[0]
+                        onto_scores, onto_consequents = self.get_consequents(domains[0], label, ontology_type=kb)
+                        tweet_scores, tweet_consequents = self.get_consequents(domains[0], label, ontology_type=-1 * kb)
+                        consequents = onto_consequents + tweet_consequents
+                        scores = onto_scores + tweet_scores
+                        arguments = self.make_linked_inferences(arguments, onto_consequents, tweet_consequents, domains[0], label, scores)
+                        for obj in zip(scores, consequents):
+                            if object2 == obj[1][0] and property == label:
+                                print("Yo ", obj[1])
+                                # save the linked inferences to the complete_arguments list with their scores to classify them later
+                                # arguments += self.classify_statements(obj[0], label)
+                            else:
+                                domains += obj[1]
 
-                if consequents:
-                    story_confirmed_by, story_denied_by = self.classify_statements(consequents, property)
+                print(arguments)
+                new_arguments = []
+                for arg in enumerate(arguments):
+                    if domains[0] != arg[1][1][-1]:
+                        new_arguments.append(arg[1])
 
-                # loop over them and check if they are in the other knowledge base:
-                for c in consequents:
-                    # if yes, check if there is a connection between the consequent and the object 2
-                    if c in knowledge_base[-1 * kb]:
-                        scores, statements = self.check_property(c, property, object2, ontology_type=kb)
-                        if statements:
-                            story_true = True
-                            story_confirmed, story_denied = self.classify_statements(consequents, property)
-                            story_confirmed_by += story_confirmed
-                            story_denied_by += story_denied
+                arguments = new_arguments
+                print(arguments)
+
+                domains.pop(0)
+
+        return arguments
 
 
     def extract_query(self, story):
@@ -185,6 +224,43 @@ class Agent:
     #         # classes_and_instances += classes_and_instances + subclasses
 
     #     return classes_and_instances
+
+
+    def get_relations(self, object1, ontology_type=1):
+        """
+        Object1 should be an owl object and property a string/label
+        Check for all possible consequents of applying the property on the object1
+        """
+
+        consequents = []
+        statement_scores = []
+        if ontology_type == 1:
+            consequents = list(default_world.sparql("""
+                                PREFIX table:<http://www.semanticweb.org/weron/ontologies/2022/8/24okt#>
+                                SELECT DISTINCT ?property
+                                { ?? rdf:type [owl:onProperty ?property] }
+                                """, [object1]))
+
+            consequents = [item for sublist in consequents for item in sublist]
+
+            statement_scores = [self.PROT_SCORE] * len(consequents)
+        else:
+            # extract consequents from mock_tweets_db
+            tweets_opened = pd.read_excel(self.tweets_source)
+
+            domains = []
+            for antec in tweets_opened["predecessor atom"]:
+                domains.append(antec.split(", "))
+
+            print("Dwadwadawd", tweets_opened['relation'])
+            for prop in enumerate(list(tweets_opened['relation'])):
+                if str(object1.label[0]).lower() in domains[prop[0]]:
+                    # calc trustworthyness
+                    statement_scores.append(0)
+                    print("kill me ", prop[1])
+                    consequents.append(prop[1]) # +prop
+
+        return statement_scores, consequents
 
 
     def get_consequents(self, object1, property, ontology_type=1):
@@ -241,6 +317,12 @@ class Agent:
 
         pass
 
+    def properties_dictionary(self):
+        self.dict_ontology_properties = {}
+        for property in self.ontology_properties_memory:
+            if property.comment:
+                self.dict_ontology_properties[property.label[0]] = property.comment[0]
+
     def update_ontology_properties_memory(self):
         # Update the ontology properties memory
         """
@@ -251,6 +333,8 @@ class Agent:
         # Add ontology properties to the memory
         self.ontology_properties_memory = self.ontology_properties_memory.union(self.ontology.properties())
         print(self.ontology_properties_memory)
+
+        self.properties_dictionary()
 
         pass
 
@@ -264,7 +348,6 @@ class Agent:
         #tweets_atoms.append(tweets_opened['successor atom'].unique())
         # add them to the memory:
         self.tweets_atoms_memory = self.tweets_atoms_memory.union(tweets_atoms)
-        print(self.tweets_atoms_memory)
 
     def update_tweets_properties_memory(self):
         # Update the tweets properties memory
